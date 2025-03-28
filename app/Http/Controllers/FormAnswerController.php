9b7eab94dba9e7a4a25ea788511ff3cfff93afc6
Hiten-benzatine <hiten@benzatine.com>
2025-03-28 09:41:12 +0530

    Refactor FormAnswerController.php by removing unnecessary blank lines

--- app/Http/Controllers/FormAnswerController.php
+++ app/Http/Controllers/FormAnswerController.php
@@ -97,11 +97,11 @@ class FormAnswerController extends Controller
         $answers = [];
     }
 
- 
+
     public function store123(Request $request)
     {
         // Retrieve the field data from the form's JSON configuration
         $fields = $request->input('fields', []);
         $answers = [];
     }
- 
+}
