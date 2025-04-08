41a10070df958b07d1baad5b0ed91625820feb5b
Hiten-benzatine <hiten@benzatine.com>
2025-04-08 10:33:49 +0530

    Remove unused store123 method from FormAnswerController

--- app/Http/Controllers/FormAnswerController.php
+++ app/Http/Controllers/FormAnswerController.php
@@ -98,11 +98,4 @@ class FormAnswerController extends Controller
     }
 
 
-
-    public function store123(Request $request)
-    {
-        // Retrieve the field data from the form's JSON configuration
-        $fields = $request->input('fields', []);
-        $answers = [];
-    }
 }
