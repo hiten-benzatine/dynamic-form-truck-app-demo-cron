b42a0d793cad3d31625b078799b26b90120f9072
Hiten-benzatine <hiten@benzatine.com>
2025-04-08 14:56:33 +0530

    Add showForm and showFormAnswers methods to FormController for displaying forms and their answers

--- app/Http/Controllers/FormAnswerController.php
+++ app/Http/Controllers/FormAnswerController.php
@@ -110,13 +110,4 @@ class FormAnswerController extends Controller
     {
         return view('forms.answers');
     }
-
-
-
-    public function store456(Request $request)
-    {
-        // Retrieve the field data from the form's JSON configuration
-        $fields = $request->input('fields', []);
-        $answers = [];
-    }                                                                                                                   
 }
